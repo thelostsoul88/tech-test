762bf4ec4395611f806566915e1180cd71d43b97
thelostsoul88 <demon.com54@gmail.com>
2023-05-21 20:29:25 +0300

    update: styles

--- src/pages/Tweets.jsx
+++ src/pages/Tweets.jsx
@@ -4,7 +4,7 @@ import { TweetsList } from "../components/TweetsList/TweetsList";
 
 export const Tweets = () => {
   return (
-    <>
+    <div className="container">
       <Link to="/" className="ml-2 fixed z-10">
         <button className="mt-8 mb-6">
           <span className="font-light font-mono flex items-center gap-1">
@@ -14,6 +14,6 @@ export const Tweets = () => {
         </button>
       </Link>
       <TweetsList />
-    </>
+    </div>
   );
 };
